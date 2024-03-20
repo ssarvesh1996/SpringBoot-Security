4c5cefb2c5708ab40e07fe643fe370e449ece5ea
“[sarvesh <[ssarvesh1996@gmail.com]>
2024-03-20 22:39:00 +0530

    Spring Boot

---
+++ src/test/java/testproject/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package testproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
